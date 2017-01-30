7a1d9ea52c5b2e54f16bbcc5fbb00397d26382dc
Daisuke Fujita <dtanshi45@gmail.com>
2017-01-30 11:57:20 +0900

    Create simple HTTP server

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("hello")
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "stimer-dashboard")
+	})
+
+	r.Run(":8080")
 }
